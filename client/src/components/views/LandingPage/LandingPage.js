095efb1f76a0987b78ffed2a3018792af3a5eaeb
jang-hoon <wkdal904@naver.com>
2021-07-17 20:11:19 +0900

    final

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, {useEffect } from 'react'
 import axios from 'axios';
-function LandingPage() {
+import {withRouter} from 'react-router-dom';
+//history를 쓰기 위한 withRouter
+function LandingPage(props) {
 
 
     useEffect(()=> {
@@ -8,14 +10,29 @@ function LandingPage() {
         .then(response=>{console.log(response)})
 
     }, [])
+
+    const onClickHandler = () =>{
+        axios.get('/api/users/logout')
+        .then(response=>{
+            if(response.data.success){
+                props.history.push("/login")
+            }else{
+                alert('로그아웃 실패')
+            }
+        })
+    }
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center'
             , width: '100%', height: '100vh'
         }}>
         <h2>시작 페이지</h2>
+
+        <button onClick={onClickHandler}>
+            로그아웃
+        </button>
         </div>
     )
 }
 
-export default LandingPage
+export default withRouter(LandingPage)
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,11 @@
+
 import React, {useState}  from 'react'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import {withRouter} from 'react-router-dom';
 
-function LoginPage() {
-
+function LoginPage(props) {
+    const dispatch = useDispatch();
 
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
@@ -11,7 +15,29 @@ function LoginPage() {
     const onPasswordHandler= (event) => {
         setPassword(event.currentTarget.value)
     }
-    const onSubmitHandler = (event)
+    const onSubmitHandler = (event)=>{
+        event.preventDefault();
+        //이게 없다면 아무것도하지않고 로그인만 눌러도
+        //페이지가 refresh가 되어버려서 원래 해야할 일들을 할 수 없어진다.
+
+        let body ={
+            email : Email,
+            password : Password
+        }
+
+        dispatch(loginUser(body))
+        .then(response=> {
+            if(response.payload.loginSuccess){
+                props.history.push('/')
+                //루트페이지로 가게해준다
+                //이때 props를 LoginPage의 파라미터로 넣어줘야한다
+            }else{
+                alert('Error')
+            }
+        })
+
+       
+    }
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center'
@@ -25,7 +51,7 @@ function LoginPage() {
             <input type="password" value={Password} onChange={onPasswordHandler} />
 
             <br />
-            <button>
+            <button type="submit">
                 Login
             </button>
         </form>
@@ -33,4 +59,4 @@ function LoginPage() {
     )
 }
 
-export default LoginPage
+export default withRouter(LoginPage)
